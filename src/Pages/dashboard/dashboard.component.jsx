1f5316bda7cc0afb6c49377339c36865783ccbda
VGrekd <virgiliogaca@outlook.com>
2021-12-22 08:31:39 -0600

    Added first page, added header navigator and first steps

---
+++ src/Pages/dashboard/dashboard.component.jsx
@@ -0,0 +1,12 @@
+import Carousel from "../../Components/carousel/carousel.component"
+import ResponsiveAppbar from "../../Components/common/appbar/responsive-app-bar.component"
+
+const Dashboard = () => {
+    return(
+        <>
+            <ResponsiveAppbar />
+        </>
+    )
+}
+
+export default Dashboard
